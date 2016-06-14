95e5b85243fba124878f9ddac979331debfb50a3
koskapan <koskapan@gmail.com>
2016-06-14 14:02:35 +0300

    Added category processing to List action

---
+++ GameStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,26 @@
+﻿using GameStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+
+        IGameRepository gameRepo;
+
+        public NavController(IGameRepository repository)
+        {
+            gameRepo = repository;
+        }
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = gameRepo.Games.Select(g => g.Category).Distinct().OrderBy(c => c);
+            return PartialView(categories);
+        }
+    }
+}
