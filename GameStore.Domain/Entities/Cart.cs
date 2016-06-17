59edf82fc1188870c44421447e111db00ad70ccc
koskapan <koskapan@gmail.com>
2016-06-17 10:15:16 +0300

    Added cart class

--- GameStore.Domain/Entities/Cart.cs
+++ GameStore.Domain/Entities/Cart.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +8,51 @@ namespace GameStore.Domain.Entities
 {
     public class Cart
     {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Game game, int quantity)
+        {
+            CartLine existingLine = lineCollection.Where(lc => lc.Game.GameId == game.GameId).FirstOrDefault();
+            if (existingLine != null)
+            {
+                existingLine.Quantity += quantity;
+            }
+            else
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Quantity = quantity,
+                    Game = game
+                });
+            }
+        }
+
+        public void RemoveLine(Game game)
+        {
+            lineCollection.RemoveAll(l => l.Game.GameId == game.GameId);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(l => l.Game.Price * l.Quantity);
+        }
+
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
     }
+
+    public class CartLine
+    {
+        public Game Game { get; set; }
+
+        public int Quantity { get; set; }
+    }
+
 }
