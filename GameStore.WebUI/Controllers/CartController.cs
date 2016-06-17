45774d5c66b0ff3755c82a6a48258dae5da314ee
koskapan <koskapan@gmail.com>
2016-06-17 11:55:49 +0300

    Added Cart controller

---
+++ GameStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,51 @@
+﻿using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        IGameRepository gameRepo;
+
+        public CartController(IGameRepository gameRepository)
+        {
+            gameRepo = gameRepository;
+        }
+
+        public RedirectToRouteResult AddToCart(int gameId, string returnUrl)
+        {
+            Game game = gameRepo.Games.FirstOrDefault(g => g.GameId == gameId);
+            if (game != null)
+            {
+                GetCart().AddItem(game, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int gameId, string returnUrl)
+        {
+            Game game = gameRepo.Games.FirstOrDefault(g => g.GameId == gameId);
+            if (game != null)
+            {
+                GetCart().RemoveLine(game);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
