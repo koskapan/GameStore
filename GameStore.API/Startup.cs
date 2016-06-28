1be72f904cbe238b2b81c91cffb346d2ef710baf
koskapan <koskapan@gmail.com>
2016-06-28 11:16:47 +0300

    I got as far as I'm stupid , remembered the built-in ASP.NET Core IoC and used it

--- GameStore.API/Startup.cs
+++ GameStore.API/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using Moq;
 
 namespace GameStore.Api
 {
@@ -35,8 +38,25 @@ namespace GameStore.Api
         {
             // Add framework services.
             services.AddApplicationInsightsTelemetry(Configuration);
-
+            services.AddTransient<IGameRepository>(s => CreateRepositoryMock());
             services.AddMvc();
+        }
+
+        private IGameRepository CreateRepositoryMock()
+        {
+            Mock<IGameRepository> gameRepoMock = new Mock<IGameRepository>();
+            gameRepoMock.Setup(g => g.Games).Returns(new List<Game>
+            {
+                new Game() { GameId = 1, Name = "Game1", Description = "Some game 1", Price = 123, Category = "Cat1" },
+                new Game() { GameId = 2, Name = "Game2", Description = "Some game 2", Price = 123, Category = "Cat2" },
+                new Game() { GameId = 3, Name = "Game3", Description = "Some game 3", Price = 123, Category = "Cat3" },
+                new Game() { GameId = 4, Name = "Game4", Description = "Some game 4", Price = 123, Category = "Cat3" },
+                new Game() { GameId = 5, Name = "Game5", Description = "Some game 5", Price = 123, Category = "Cat2" },
+                new Game() { GameId = 6, Name = "Game6", Description = "Some game 6", Price = 123, Category = "Cat1" },
+                new Game() { GameId = 7, Name = "Game7", Description = "Some game 7", Price = 123, Category = "Cat2" },
+                new Game() { GameId = 8, Name = "Game8", Description = "Some game 8", Price = 123, Category = "Cat3" }
+            });
+            return gameRepoMock.Object;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
